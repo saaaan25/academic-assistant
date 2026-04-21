c43acf7ae7dca671c17b7b9485fea94984f7eb30
FranzARCH <franzchipana2610@gmail.com>
2026-04-20 23:20:37 -0500

    feat: add comment

--- agent/admin.py
+++ agent/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Document, ChatSession, Message, Citation
 
+# Agregar modelos al panel de admin
 admin.site.register(Document)
 admin.site.register(ChatSession)
 admin.site.register(Message)
